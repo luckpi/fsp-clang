1f1f140dea09443b848ddf9c11a9b1a394570c68
jdhxyy <jdh821@163.com>
2023-07-06 23:06:06 +0800

    增加cache解决占用内存过大的问题

--- fsp.h
+++ fsp.h
@@ -4,35 +4,34 @@
 #ifndef _FSP_H
 #define _FSP_H
 
-#include "async.h"
-#include "crc16.h"
-#include "lagan.h"
-#include "pt.h"
-#include "stdbool.h"
-#include "stddef.h"
-#include "stdint.h"
-#include "string.h"
-#include "tzfifo.h"
-#include "tzlist.h"
-#include "tzmalloc.h"
-#include "tztime.h"
 #include "tztype.h"
 
-// FspLoad Fsp载入
-// mallocTotal malloc内存大小
-// frameMaxLen 最大帧长
-// fifoItemSum fifo元素和
-// timeoutM 超时时间, 单位: Ms
-bool FspLoad(int mallocTotal, int frameMaxLen, int fifoItemSum, uint64_t timeout);
+// malloc空间字节数
+#define FSP_MALLOC_SIZE 20480
 
-// ToolGetTxBytes 读取发送字节流
-// 注意:谁调用谁释放空间
-TZBufferDynamic *FspGetTxBytes(uint8_t *data, int dataLen, bool isNeedCrc);
+// FspLoad 模块载入
+bool FspLoad(int pipeNum);
 
-// FspReceive Fsp接收
-void FspReceive(uint8_t *data, int dataLen, int pipe, uint32_t ip, uint16_t port);
+// FspAddPipe 增加管道.过期时间单位:ms
+bool FspAddPipe(int pipe, int frameLenMax, int expireTime, int fifoCache);
+
+// FspReceive 接收FSP帧
+void FspReceive(uint8_t* data, int dataLen, int pipe, uint32_t ip, uint16_t port);
 
 // FspRegisterObserver 注册Fsp观察者
 bool FspRegisterObserver(TZPipeDataFunc callback);
+
+// FspBytesToFrame 字节流转FSP帧
+// 返回值是FSP帧字节数.返回0表示转换失败.帧存储在dst中
+// 如果src长度足够长,可以设置dst等于src,这样转换后的帧会存放在src中
+int FspBytesToFrame(uint8_t *src, int srcLen, bool isNeedCrc, uint8_t* dst, int dstSize);
+
+// FspFrameToBytes FSP帧转换为字节流
+// 返回值是字节流字节数.返回0表示转换失败.字节流存储在dst中
+// 如果不需要src帧,为节约空间,可将dst设置为src的地址
+int FspFrameToBytes(uint8_t *src, int srcLen, bool isNeedCrc, uint8_t* dst, int dstSize);
+
+// FspIsFrameValid 是否有效的FSP帧
+bool FspIsFrameValid(uint8_t* frame, int len);
 
 #endif
